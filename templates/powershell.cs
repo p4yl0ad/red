19cd37f907627ad7b1ac37cd5d9db9ae301128af
p4yl0ad <57832389+p4yl0ad@users.noreply.github.com>
2021-06-02 10:17:11 +0100

    Update powershell.cs

--- templates/powershell.cs
+++ templates/powershell.cs
@@ -10,7 +10,8 @@ namespace Oops
 	{
 		static void Main(string[] args)
 		{
-      //Move System.Management.Automation.dll into directory
+			//https://docs.microsoft.com/en-us/powershell/scripting/developer/hosting/adding-and-invoking-commands?view=powershell-7.1
+      			//Move System.Management.Automation.dll into directory
 			//C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe /reference:System.Management.Automation.dll /platform:x64 /t:exe /unsafe /out:\rastalabs\payloads\oops.exe C:\rastalabs\payloads\oops.cs
 			PowerShell ps = PowerShell.Create().AddCommand ("cmd.exe").AddParameter("/c", "calc.exe");
 			ps.Invoke();

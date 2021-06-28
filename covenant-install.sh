bd33de6c9833da2ef1614110334e5f5d70127f38
p4yl0ad <57832389+p4yl0ad@users.noreply.github.com>
2021-06-28 03:26:44 +0100

    Update covenant-install.sh

--- covenant-install.sh
+++ covenant-install.sh
@@ -1,22 +1,19 @@
 #!/usr/bin/bash
 
-
 rm -rf /opt/Covenant 2>/dev/null
 rm -rf /opt/runtime.tar.gz 2>/dev/null
+sudo apt remove dotnet* -y 2>/dev/null
+sudo apt remove aspnetcore* -y 2>/dev/null
 
-sudo apt remove dotnet* -y
-sudo apt remove aspnetcore* -y
-
-
-wget https://packages.microsoft.com/config/debian/10/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-sudo dpkg -i packages-microsoft-prod.deb
+wget https://packages.microsoft.com/config/debian/10/packages-microsoft-prod.deb -O /opt/packages-microsoft-prod.deb
+sudo dpkg -i /opt/packages-microsoft-prod.deb
 
 sudo apt-get update -y; \
 	sudo apt-get install -y apt-transport-https && \
 	sudo apt-get update && \
 	sudo apt-get install -y dotnet-sdk-3.1 aspnetcore-runtime-3.1 dotnet-runtime-3.1
 
-git clone --recurse-submodules https://github.com/cobbr/Covenant
+cd /opt; git clone --recurse-submodules https://github.com/cobbr/Covenant
 cd /opt/Covenant/Covenant
 /usr/bin/dotnet clean
 /usr/bin/dotnet build

